fe158ab49bc41b24e98c6ee08160d2eb8a1a3376
ywg <281491130@qq.com>
2018-07-02 01:45:11 +0800

    111

--- httpclient/src/main/java/com/ywg/httpclient/HttpClient.java
+++ httpclient/src/main/java/com/ywg/httpclient/HttpClient.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -18,7 +17,6 @@ import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
-import org.apache.http.util.EntityUtils;
 import org.junit.Test;
 
 public class HttpClient {
@@ -33,6 +31,7 @@ public class HttpClient {
 	}
 	@Test
 	public void name() {
+		
 		initConnectPool();
 		Thread[] threads=new Thread[50];
 		for (int i = 0; i < threads.length; i++) {
