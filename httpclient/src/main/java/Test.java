373ede9e5f9c7e4447482101489c70c12dac5901
yangwgs <yangwgs@yonyou.com>
2019-02-18 20:02:51 +0800

    1123

--- httpclient/src/main/java/Test.java
+++ httpclient/src/main/java/Test.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Test {
+	
 	public static void main(String[] args) {
 		Scanner input=new Scanner(System.in);
 		String string=input.nextLine();
